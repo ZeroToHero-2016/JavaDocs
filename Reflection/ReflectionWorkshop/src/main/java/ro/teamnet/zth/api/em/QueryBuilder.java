e488c7d1dd2b93603d0677cbed252053bf7fece0
Alexandra Lazea <medeealazea@yahoo.com>
2016-07-07 23:35:33 +0300

    Reflection - continuare

---
+++ Reflection/ReflectionWorkshop/src/main/java/ro/teamnet/zth/api/em/QueryBuilder.java
@@ -0,0 +1,31 @@
+package ro.teamnet.zth.api.em;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class QueryBuilder {
+
+    private Object tableName;
+    private List<ColumnInfo> queryColumns;
+    private QueryType queryType;
+    private List<Condition> conditions;
+
+
+    public String getValueForQuery(Object value){
+        if (value.getClass().equals(String.class)){
+            return (String)value;
+        }
+        if (value.getClass().equals(Date.class)){
+            DateFormat dateFormat = new SimpleDateFormat("mm/dd/yyyy"); 
+            return "TO_DATE('" + dateFormat.format((Date)value) + "','mm-dd-yyyy'";
+        }
+        return "";
+    }
+
+    public QueryBuilder addCondition(Condition condition){
+        QueryBuilder query = new QueryBuilder();
+        
+    }
+}
--- Reflection/ReflectionWorkshop/src/test/java/ro/teamnet/zth/api/em/EntityUtilsTest.java
+++ Reflection/ReflectionWorkshop/src/test/java/ro/teamnet/zth/api/em/EntityUtilsTest.java
@@ -3,6 +3,7 @@ package ro.teamnet.zth.api.em;
 import org.junit.Test;
 import ro.teamnet.zth.api.annotations.Column;
 import ro.teamnet.zth.appl.domain.Department;
+import ro.teamnet.zth.appl.domain.Location;
 
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
@@ -29,13 +30,21 @@ public class EntityUtilsTest {
     public void testcastFromSqlTypeMethod() {
         BigDecimal value = new BigDecimal(1.0);
         Object o = EntityUtils.castFromSqlType(value, Integer.class);
-        assertEquals("Table name should be departments!", Integer.class, o.getClass());
+        assertEquals("Method should return Integer!", Integer.class, o.getClass());
+        o = EntityUtils.castFromSqlType(value,Float.class);
+        assertEquals("Method should return Float!", Float.class, o.getClass());
+        o = EntityUtils.castFromSqlType(value,Long.class);
+        assertEquals("Method should return Long!", Long.class, o.getClass());
+        o = EntityUtils.castFromSqlType(value,Double.class);
+        assertEquals("Method should return Float!", Double.class, o.getClass());
     }
 
     @Test
     public void testgetFieldsByAnnotationsMethod() {
         List<Field> tableName = EntityUtils.getFieldsByAnnotations(Department.class, Column.class);
-        assertEquals("Departments has 2 columns.", 2, tableName.size());
+        assertEquals("departments has 2 columns.", 2, tableName.size());
+        tableName = EntityUtils.getFieldsByAnnotations(Location.class, Column.class);
+        assertEquals("locations has 5 columns.", 4, tableName.size());
     }
 
     @Test
@@ -43,7 +52,7 @@ public class EntityUtilsTest {
         Department d = new Department();
         d.setId(12);
         Object tableName = EntityUtils.getSqlValue(d);
-        assertEquals("Departments has 2 columns.", (long)12, tableName);
+        assertEquals("Id isn't correct.", (long)12, tableName);
     }
 
 }
